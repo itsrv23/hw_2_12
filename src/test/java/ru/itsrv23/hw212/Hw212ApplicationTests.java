6aca2130f52ce811d0cd3bcd6204ecd909ceee5b
itsrv23 <nifirus@gmail.com>
2022-05-07 09:18:26 +0300

    init

---
+++ src/test/java/ru/itsrv23/hw212/Hw212ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itsrv23.hw212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw212ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
